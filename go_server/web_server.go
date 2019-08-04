32b2c2533f1f3576e6ae173864b7f2628687035b
cbmathieson <cbmathieson@outlook.com>
2019-08-04 16:01:08 -0700

    organizing

--- go_server/web_server.go
+++ go_server/web_server.go
@@ -40,26 +40,171 @@ var quadrantLonSize float64 = -1
 var totalInstances float64 = 0
 var sideLength int
 
-func newPool(port int) *redis.Pool {
+func main() {
 
-	portString := fmt.Sprintf(":%d", port)
+	redisInstances, err := initPools()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	return &redis.Pool{
-		//Max number of idle connections in the pool
-		MaxIdle: 80,
-		//Max number of connections
-		MaxActive: 12000,
+	redisQueues := makeQueues()
 
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", portString)
+	fmt.Println(redisInstances)
+	fmt.Println(redisQueues)
+
+	// HAVE NOT IMPLEMENTED QUEUING - JUST FOR TESTING
+
+	// generates incoming notes
+	go func() {
+		for {
+			time.Sleep(1000 * time.Millisecond)
+
+			// creates artificial note
+			note := generateNote()
+
+			// uses location passed by note to decide what redis instance to pass to
+			x, y := findInstanceInRadius(note.Longitude, note.Latitude)
+
+			// adds note to redis instance
+			err = addNote(note, redisInstances[x][y])
 			if err != nil {
-				panic(err.Error())
+				// add back to queue
 			}
-			return c, err
-		},
+		}
+	}()
+
+	// waits for read requests
+	for {
+
+		time.Sleep(1000 * time.Millisecond)
+
+		// get artificial coordinates for a user
+		userLon, userLat := getRandomCoordinates()
+
+		notes := readRequest(userLon, userLat, redisInstances)
+
+		fmt.Println(len(notes))
+
 	}
 }
 
+// ------------------ redis interactions ------------------
+
+// adds note to map
+func addNote(note Note, pool *redis.Pool) error {
+
+	// create connection
+	c := pool.Get()
+	defer c.Close()
+
+	//marshal into json
+	b, err := json.Marshal(note)
+	if err != nil {
+		return err
+	}
+
+	//fmt.Println(string(b))
+	input := string(b)
+
+	fmt.Printf("note lat: %f, lon: %f\n", note.Latitude, note.Longitude)
+
+	err = GEOADD(c, note, input)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// generates 2D slice of redis instances based on ports given
+func createPools(ports []int) [][]*redis.Pool {
+	redisInstances := make([][]*redis.Pool, sideLength)
+
+	for i := 0; i < sideLength; i++ {
+		redisInstances[i] = make([]*redis.Pool, sideLength)
+		for j := 0; j < sideLength; j++ {
+			redisInstances[i][j] = newPool(ports[i])
+
+			// just checking if connected to instance
+			conn := redisInstances[i][j].Get()
+			ping(conn)
+			conn.Close()
+		}
+	}
+
+	return redisInstances
+}
+
+// handles user request for notes in area
+func readRequest(lon float64, lat float64, redisInstances [][]*redis.Pool) []Note {
+
+	x, y := findInstanceInRadius(lon, lat)
+
+	pool := redisInstances[x][y]
+
+	c := pool.Get()
+	defer c.Close()
+
+	fmt.Printf("searching 21,000km radius from lat: %f, lon: %f in instance x:%d,y:%d\n", lon, lat, x, y)
+
+	reply := GEORADIUS(c, lon, lat, "21000", "km")
+
+	var notes []Note
+
+	switch t := reply.(type) {
+	case []interface{}:
+		returnedValues := make([]Note, len(t))
+		for i, value := range t {
+			if err := json.Unmarshal(value.([]byte), &returnedValues[i]); err != nil {
+				panic(err)
+			}
+		}
+		notes = returnedValues
+	default:
+		fmt.Println("uh oh not a data type we wanted")
+	}
+
+	return notes
+
+}
+
+//initialises redis instances and sets global variables
+func initPools() ([][]*redis.Pool, error) {
+
+	dbPort, redisPorts := getPorts()
+
+	fmt.Printf("dbPort: %d\n", dbPort)
+	fmt.Printf("redisPorts: %v\n", redisPorts)
+
+	//check if power of 4
+	if !powerOf4(len(redisPorts)) {
+		errorString := fmt.Sprintf("ERROR: Can't split %d instances into even quadrants. Need 4^n instances\n", len(redisPorts))
+		return nil, errors.New(errorString)
+	}
+
+	//creating quadrants based on number of redis instances
+	totalLat := float64(85 + 85)
+	totalLon := float64(180 + 180)
+
+	sideLength = int(math.Sqrt(float64(len(redisPorts))))
+	totalInstances = float64(len(redisPorts))
+	quadrantLatSize = totalLat / float64(sideLength)
+	quadrantLonSize = totalLon / float64(sideLength)
+
+	fmt.Printf("lat: %f, lon: %f per quadrant\n", quadrantLatSize, quadrantLonSize)
+
+	redisInstances := createPools(redisPorts)
+
+	return redisInstances, nil
+
+}
+
+// ---------------------------------------------------
+
+// ------------------ Utilities ------------------
+
+// creates an artificial note
 func generateNote() Note {
 	lon, lat := getRandomCoordinates()
 	pid++
@@ -78,31 +223,7 @@ func generateNote() Note {
 	}
 }
 
-func addNote(note Note, pool *redis.Pool) error {
-
-	// create connection
-	c := pool.Get()
-	defer c.Close()
-
-	//marshal into json
-	b, err := json.Marshal(note)
-	if err != nil {
-		return err
-	}
-
-	//fmt.Println(string(b))
-	input := string(b)
-
-	fmt.Printf("note lat: %f, lon: %f\n", note.Latitude, note.Longitude)
-
-	_, err = c.Do("GEOADD", "mapNotes", note.Longitude, note.Latitude, input)
-
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
+// generates random lon,lat values
 func getRandomCoordinates() (float64, float64) {
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -139,8 +260,161 @@ func getRandomCoordinates() (float64, float64) {
 
 }
 
-func geoAdd(c redis.Conn) error {
-	_, err := c.Do("GEOADD", "Favor")
+// returns dbport, and redis ports.
+// if empty, exit
+func getPorts() (int, []int) {
+
+	// Read in db port
+
+	dbFile, err := os.Open("../ports/db.txt")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("ERROR: no db port")
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(dbFile)
+
+	dbPort := 0
+
+	for scanner.Scan() {
+		if dbPort != 0 {
+			break
+		}
+		line := scanner.Text()
+		dbPort, err = strconv.Atoi(line)
+
+		if err != nil {
+			fmt.Println("ERROR: could not parse string in file")
+			os.Exit(1)
+		}
+	}
+
+	if dbPort == 0 {
+		fmt.Println("ERROR: No port listening for db")
+		os.Exit(1)
+	}
+
+	// Read in redis ports
+
+	redisFile, err := os.Open("../ports/redis.txt")
+
+	if err != nil {
+		fmt.Println("ERROR: no redis port")
+		os.Exit(1)
+	}
+
+	scanner = bufio.NewScanner(redisFile)
+
+	redisPorts := make([]int, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		port, err := strconv.Atoi(line)
+
+		if err != nil {
+			fmt.Println("ERROR: could not parse string in file")
+			os.Exit(1)
+		}
+
+		redisPorts = append(redisPorts, port)
+	}
+
+	if len(redisPorts) < 1 {
+		fmt.Println("ERROR: no redis ports")
+		os.Exit(1)
+	}
+
+	return dbPort, redisPorts
+
+}
+
+// checks if values is a power of 4
+func powerOf4(instances int) bool {
+	if instances == 0 {
+		return false
+	}
+	for instances != 1 {
+		if instances%4 != 0 {
+			return false
+		}
+		instances = instances / 4
+	}
+	return true
+}
+
+// returns array index of redis index in user's region
+// lon maps to x and lat maps to y
+// returns -1 if failure
+func findInstanceInRadius(lon float64, lat float64) (int, int) {
+	var xIndex = -1
+	var yIndex = -1
+	for i := 0; i < sideLength; i++ {
+		if lat >= (-85 + (quadrantLatSize * float64(i))) {
+			yIndex = i
+		}
+	}
+
+	for i := 0; i < sideLength; i++ {
+		if lon >= (-180 + (quadrantLonSize * float64(i))) {
+			xIndex = i
+		}
+	}
+
+	return xIndex, yIndex
+}
+
+//returns Query with {-1,""} if empty
+func dequeue(queue []Query) ([]Query, Query) {
+	if len(queue) <= 0 {
+		return queue, Query{-1, ""}
+	}
+	//peek top
+	x := queue[0]
+	//delete top
+	queue = queue[1:]
+
+	return queue, x
+}
+
+// creates 2D slice of queues that map to each redis instance using 'findInstanceInRadius()'
+func makeQueues() [][][]Query {
+	redisQueues := make([][][]Query, sideLength)
+	for i := range redisQueues {
+		redisQueues[i] = make([][]Query, sideLength)
+		for j := range redisQueues[i] {
+			redisQueues[i][j] = make([]Query, 0)
+		}
+	}
+	return redisQueues
+}
+
+// -----------------------------------------------
+
+// ------------------ redis functions ------------------
+
+// connects to the redis instance located at the given port
+func newPool(port int) *redis.Pool {
+
+	portString := fmt.Sprintf(":%d", port)
+
+	return &redis.Pool{
+		//Max number of idle connections in the pool
+		MaxIdle: 80,
+		//Max number of connections
+		MaxActive: 12000,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", portString)
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
+}
+
+func GEOADD(c redis.Conn, note Note, input string) error {
+	_, err := c.Do("GEOADD", "mapNotes", note.Longitude, note.Latitude, input)
 	return err
 }
 
@@ -219,266 +493,12 @@ func DEL(c redis.Conn, key string) bool {
 	return resp == 1
 }
 
-func GEORADIUS(c redis.Conn, key string, lon float64, lat float64, radius string, unit string) interface{} {
-	resp, err := c.Do("GEORADIUS", key, lon, lat, "21000", "km")
+func GEORADIUS(c redis.Conn, lon float64, lat float64, radius string, unit string) interface{} {
+	resp, err := c.Do("GEORADIUS", "mapNotes", lon, lat, radius, unit)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return resp
 }
 
-// returns dbport, and redis ports.
-// if empty, exit
-func getPorts() (int, []int) {
-
-	// Read in db port
-
-	dbFile, err := os.Open("../ports/db.txt")
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("ERROR: no db port")
-		os.Exit(1)
-	}
-	scanner := bufio.NewScanner(dbFile)
-
-	dbPort := 0
-
-	for scanner.Scan() {
-		if dbPort != 0 {
-			break
-		}
-		line := scanner.Text()
-		dbPort, err = strconv.Atoi(line)
-
-		if err != nil {
-			fmt.Println("ERROR: could not parse string in file")
-			os.Exit(1)
-		}
-	}
-
-	if dbPort == 0 {
-		fmt.Println("ERROR: No port listening for db")
-		os.Exit(1)
-	}
-
-	// Read in redis ports
-
-	redisFile, err := os.Open("../ports/redis.txt")
-
-	if err != nil {
-		fmt.Println("ERROR: no redis port")
-		os.Exit(1)
-	}
-
-	scanner = bufio.NewScanner(redisFile)
-
-	redisPorts := make([]int, 0)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		port, err := strconv.Atoi(line)
-
-		if err != nil {
-			fmt.Println("ERROR: could not parse string in file")
-			os.Exit(1)
-		}
-
-		redisPorts = append(redisPorts, port)
-	}
-
-	if len(redisPorts) < 1 {
-		fmt.Println("ERROR: no redis ports")
-		os.Exit(1)
-	}
-
-	return dbPort, redisPorts
-
-}
-
-func createPools(ports []int) [][]*redis.Pool {
-	redisInstances := make([][]*redis.Pool, sideLength)
-
-	for i := 0; i < sideLength; i++ {
-		redisInstances[i] = make([]*redis.Pool, sideLength)
-		for j := 0; j < sideLength; j++ {
-			redisInstances[i][j] = newPool(ports[i])
-
-			// just checking if connected to instance
-			conn := redisInstances[i][j].Get()
-			ping(conn)
-			conn.Close()
-		}
-	}
-
-	return redisInstances
-}
-
-func powerOf4(instances int) bool {
-	if instances == 0 {
-		return false
-	}
-	for instances != 1 {
-		if instances%4 != 0 {
-			return false
-		}
-		instances = instances / 4
-	}
-	return true
-}
-
-// returns array index of redis index in user's region
-// lon maps to x and lat maps to y
-// returns -1 if failure
-func findInstanceInRadius(lon float64, lat float64) (int, int) {
-	var xIndex = -1
-	var yIndex = -1
-	for i := 0; i < sideLength; i++ {
-		if lat >= (-85 + (quadrantLatSize * float64(i))) {
-			yIndex = i
-		}
-	}
-
-	for i := 0; i < sideLength; i++ {
-		if lon >= (-180 + (quadrantLonSize * float64(i))) {
-			xIndex = i
-		}
-	}
-
-	return xIndex, yIndex
-}
-
-func readRequest(lon float64, lat float64, redisInstances [][]*redis.Pool) []Note {
-
-	x, y := findInstanceInRadius(lon, lat)
-
-	pool := redisInstances[x][y]
-
-	c := pool.Get()
-	defer c.Close()
-
-	fmt.Printf("searching 21,000km radius from lat: %f, lon: %f in instance x:%d,y:%d\n", lon, lat, x, y)
-
-	reply := GEORADIUS(c, "mapNotes", lon, lat, "21000", "km")
-
-	var notes []Note
-
-	switch t := reply.(type) {
-	case []interface{}:
-		returnedValues := make([]Note, len(t))
-		for i, value := range t {
-			if err := json.Unmarshal(value.([]byte), &returnedValues[i]); err != nil {
-				panic(err)
-			}
-		}
-		notes = returnedValues
-	default:
-		fmt.Println("uh oh not a data type we wanted")
-	}
-
-	return notes
-
-}
-
-//returns Query with {-1,""} if empty
-func dequeue(queue []Query) ([]Query, Query) {
-	if len(queue) <= 0 {
-		return queue, Query{-1, ""}
-	}
-	//peek top
-	x := queue[0]
-	//delete top
-	queue = queue[1:]
-
-	return queue, x
-}
-
-func makeQueues() [][][]Query {
-	redisQueues := make([][][]Query, sideLength)
-	for i := range redisQueues {
-		redisQueues[i] = make([][]Query, sideLength)
-		for j := range redisQueues[i] {
-			redisQueues[i][j] = make([]Query, 0)
-		}
-	}
-	return redisQueues
-}
-
-//initialises redis instances and sets global variables
-func initPools() ([][]*redis.Pool, error) {
-
-	dbPort, redisPorts := getPorts()
-
-	fmt.Printf("dbPort: %d\n", dbPort)
-	fmt.Printf("redisPorts: %v\n", redisPorts)
-
-	//check if power of 4
-	if !powerOf4(len(redisPorts)) {
-		errorString := fmt.Sprintf("ERROR: Can't split %d instances into even quadrants. Need 4^n instances\n", len(redisPorts))
-		return nil, errors.New(errorString)
-	}
-
-	//creating quadrants based on number of redis instances
-	totalLat := float64(85 + 85)
-	totalLon := float64(180 + 180)
-
-	sideLength = int(math.Sqrt(float64(len(redisPorts))))
-	totalInstances = float64(len(redisPorts))
-	quadrantLatSize = totalLat / float64(sideLength)
-	quadrantLonSize = totalLon / float64(sideLength)
-
-	fmt.Printf("lat: %f, lon: %f per quadrant\n", quadrantLatSize, quadrantLonSize)
-
-	redisInstances := createPools(redisPorts)
-
-	return redisInstances, nil
-
-}
-
-func main() {
-
-	redisInstances, err := initPools()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	redisQueues := makeQueues()
-
-	fmt.Println(redisInstances)
-	fmt.Println(redisQueues)
-
-	// HAVE NOT IMPLEMENTED QUEUING - JUST FOR TESTING
-
-	// generates incoming notes
-	go func() {
-		for {
-			time.Sleep(1000 * time.Millisecond)
-
-			// creates artificial note
-			note := generateNote()
-
-			// uses location passed by note to decide what redis instance to pass to
-			x, y := findInstanceInRadius(note.Longitude, note.Latitude)
-
-			// adds note to redis instance
-			err = addNote(note, redisInstances[x][y])
-			if err != nil {
-				// add back to queue
-			}
-		}
-	}()
-
-	// waits for read requests
-	for {
-
-		time.Sleep(1000 * time.Millisecond)
-
-		// get artificial coordinates for a user
-		userLon, userLat := getRandomCoordinates()
-
-		notes := readRequest(userLon, userLat, redisInstances)
-
-		fmt.Println(len(notes))
-
-	}
-}
+// -----------------------------------------------
